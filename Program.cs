e8211cd16433d911f5c30ac6e66d2a015cd3d8ad
AndreyKerges <andrey.nascimento4@etec.sp.gov.br>
2023-03-17 22:08:05 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Qual o seu nome?");
+string nome = Console.ReadLine()!;
+
+Console.Write("Qual o seu sobrenome?");
+string sobrenome = Console.ReadLine()!;
+
+Console.Write($"Boa noite, {nome} {sobrenome}.");
